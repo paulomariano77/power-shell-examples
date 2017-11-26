8ec493c3d76dfd8b25c36978311f20383b893197
Paulo Mariano <pazevedo@stone.com.br>
2017-11-26 20:02:45 -0200

    Aula 02

---
+++ aula2.ps1
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------------------------------------------
+# Caracteres de escape
+# --------------------------------------------------------------------------------------------------------------
+
+<# Tipos de caract
+0: Nulo
+a: alerta de bip no sistema
+b: apagar uma letra
+f: avançar uma pagina
+n: nova linha
+t: tab
+v: tab vertical
+r: remove todos os caracteres antes
+
+Usa-se a crase `
+#>
+
+Clear-Host
+
+Write-Host "Instituto de Script's`0"
+Write-Host "`a"
+Write-Host "Computar `bação"
+
+Write-Host "`f"
+Write-Host "Instituto `nde`nScript's"
+Write-Host "Instituto `rde Script's"
+Write-Host "Instituto `tde `tScript's"
+Write-Host "`?"
+
+
